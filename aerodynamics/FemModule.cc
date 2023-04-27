5fb3973c7012bf3d294378ccb9e814d0a3c4182b
Alexandre l'Heritier <alexandre.lheritier@cea.fr>
2023-04-27 13:37:42 +0200

    Retry with aerodynamics module

--- aerodynamics/FemModule.cc
+++ aerodynamics/FemModule.cc
@@ -95,7 +95,7 @@ void FemModule::
 compute()
 {
   info() << "Module Fem COMPUTE";
-  info() << "Very good edit !!!";
+  info() << "Retry";
 
   // Stop code after computations
   if (m_global_iteration() > 0)
