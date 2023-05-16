52f223841d10593882b5ec8fc84a000716e19e94
Afeef <afeef.badri@gmail.com>
2023-05-16 17:14:18 +0200

    fixes #77

--- soildynamics/FemModule.cc
+++ soildynamics/FemModule.cc
@@ -928,13 +928,13 @@ $$
         if (!(m_u2_fixed[node]) && node.isOwn()) {
           DoFLocalId dof_id2 = node_dof.dofId(node, 1);
           rhs_values[dof_id2] += (  c7*( cp*( Normal.x*Normal.y*m_U[node].x + Normal.y*Normal.y*m_U[node].y ) +
-                                         cs*(-Normal.x*Normal.y*m_U[node].x - Normal.x*Normal.x*m_U[node].y )
+                                         cs*(-Normal.x*Normal.y*m_U[node].x + Normal.x*Normal.x*m_U[node].y )
                                        )
                                   - c8*( cp*( Normal.x*Normal.y*m_V[node].x + Normal.y*Normal.y*m_V[node].y ) +
-                                         cs*(-Normal.x*Normal.y*m_V[node].x - Normal.x*Normal.x*m_V[node].y )
+                                         cs*(-Normal.x*Normal.y*m_V[node].x + Normal.x*Normal.x*m_V[node].y )
                                        )
                                   - c9*( cp*( Normal.x*Normal.y*m_A[node].x + Normal.y*Normal.y*m_A[node].y ) +
-                                         cs*(-Normal.x*Normal.y*m_A[node].x - Normal.x*Normal.x*m_A[node].y )
+                                         cs*(-Normal.x*Normal.y*m_A[node].x + Normal.x*Normal.x*m_A[node].y )
                                        )
                                  ) * length / 2.;
         }
