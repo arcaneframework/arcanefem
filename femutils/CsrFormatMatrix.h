0a15348f77bac220d03691122e2ff1d31502cac8
kerian.allaire <kerian.allaire@epita.fr>
2023-12-01 14:41:58 +0100

    feat : BLCSR working

--- femutils/CsrFormatMatrix.h
+++ femutils/CsrFormatMatrix.h
@@ -102,7 +102,7 @@ class CsrFormat : TraceAccessor
   {
     for (Int32 i = 0; i < m_matrix_row.dim1Size(); i++) {
       for (Int32 j = m_matrix_row(i); (i + 1 < m_matrix_row.dim1Size() && j < m_matrix_row(i + 1)) || (i + 1 == m_matrix_row.dim1Size() && j < m_matrix_column.dim1Size()); j++)
-        linear_system.matrixSetValue(DoFLocalId(i), DoFLocalId(m_matrix_column(j)), m_matrix_value(j));
+        linear_system.matrixAddValue(DoFLocalId(i), DoFLocalId(m_matrix_column(j)), m_matrix_value(j));
     }
   }
 
