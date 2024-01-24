267173570b675a71394a588fc652d5fa9c077f4a
Afeef <afeef.badri@gmail.com>
2024-01-24 15:39:54 +0100

    remove timer

--- poisson/LegacyBiliAssembly.hxx
+++ poisson/LegacyBiliAssembly.hxx
@@ -18,33 +18,13 @@ _assembleBilinearOperatorTRIA3()
 
   Timer::Action timer_action(m_time_stats, "AssembleLegacyBilinearOperatorTria3");
 
-  std::chrono::_V2::system_clock::time_point lhs_start;
-  double compute_average = 0;
-  double global_build_average = 0;
-  if (m_register_time) {
-    logger << "-------------------------------------------------------------------------------------\n"
-           << "Using hashmap legacy format\n";
-    lhs_start = std::chrono::high_resolution_clock::now();
-  }
-
   ENUMERATE_ (Cell, icell, allCells()) {
     Cell cell = *icell;
-
-    std::chrono::_V2::system_clock::time_point compute_El_start;
-    if (m_register_time) {
-      compute_El_start = std::chrono::high_resolution_clock::now();
-    }
 
     FixedMatrix<3, 3> K_e;
     {
       //Timer::Action timer_action(m_time_stats, "LegacyComputeElementMatrix");
       K_e = _computeElementMatrixTRIA3(cell); // element stifness matrix
-    }
-
-    if (m_register_time) {
-      auto compute_El_stop = std::chrono::high_resolution_clock::now();
-      std::chrono::duration<double> compute_duration = compute_El_stop - compute_El_start;
-      compute_average += compute_duration.count();
     }
 
     //             # assemble elementary matrix into the global one
@@ -55,11 +35,6 @@ _assembleBilinearOperatorTRIA3()
     //                 for node2 in elem.nodes:
     //                     inode2=elem.nodes.index(node2)
     //                     K[node1.rank,node2.rank]=K[node1.rank,node2.rank]+K_e[inode1,inode2]
-
-    std::chrono::_V2::system_clock::time_point global_build_start;
-    if (m_register_time) {
-      global_build_start = std::chrono::high_resolution_clock::now();
-    }
 
     {
       //Timer::Action timer_action(m_time_stats, "LegacyAddToGlobalMatrix");
@@ -78,28 +53,6 @@ _assembleBilinearOperatorTRIA3()
         ++n1_index;
       }
     }
-    if (m_register_time) {
-      auto global_build_stop = std::chrono::high_resolution_clock::now();
-      std::chrono::duration<double> global_build_duration = global_build_stop - global_build_start;
-      global_build_average += global_build_duration.count();
-    }
-  }
-
-  if (m_register_time) {
-    auto lhs_end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> duration = lhs_end - lhs_start;
-    double lhs_loc_time = duration.count();
-    logger << "compute elements average time : " << compute_average / nbCell() << "\n"
-           << "compute elements total time : " << compute_average << "\n"
-           << "add in global matrix average time : " << global_build_average / nbCell() << "\n"
-           << "add in global matrix total time : " << global_build_average << "\n"
-           << "lhs total time : " << lhs_loc_time << "\n"
-           << "compute element time in lhs : " << compute_average / lhs_loc_time * 100 << "%\n"
-           << "add in global matrix time in lhs : " << global_build_average / lhs_loc_time * 100 << "%\n\n"
-           << "-------------------------------------------------------------------------------------\n\n";
-    lhs_time += lhs_loc_time;
-    wbuild << lhs_loc_time << ",";
-    timer << lhs_loc_time << ",";
   }
 }
 
