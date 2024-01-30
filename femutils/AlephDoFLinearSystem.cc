fdf43170cc9033abd2a7305798aa0d28080f869e
Afeef <afeef.badri@gmail.com>
2024-01-30 18:36:53 +0100

    fix for PETSc on INTI

--- femutils/AlephDoFLinearSystem.cc
+++ femutils/AlephDoFLinearSystem.cc
@@ -258,10 +258,10 @@ class AlephDoFLinearSystemImpl
     auto* aleph_solution_vector = m_aleph_solution_vector;
     DoFGroup own_dofs = m_dof_family->allItems().own();
     const Int32 nb_dof = own_dofs.size();
-    UniqueArray<Real> vector_zero(nb_dof);
-    vector_zero.fill(0.0);
+    m_vector_zero(nb_dof);
+    m_vector_zero.fill(0.0);
 
-    aleph_solution_vector->setLocalComponents(vector_zero);
+    aleph_solution_vector->setLocalComponents(m_vector_zero);
     aleph_solution_vector->assemble();
 
     Int32 nb_iteration = 0;
@@ -407,6 +407,8 @@ class AlephDoFLinearSystemImpl
 
   //! True is we need to manually destroy the matrix/vector
   bool m_need_destroy_matrix_and_vector = true;
+
+  UniqueArray<Real> m_vector_zero;
 
   Runner* m_runner = nullptr;
 
