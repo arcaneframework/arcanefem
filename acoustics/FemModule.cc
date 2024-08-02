bbf19496b628cc5f23456fcb705d06b7d2d9221c
Mohd Afeef Badri <52162083+mohd-afeef-badri@users.noreply.github.com>
2024-08-02 11:18:25 +0200

    Clean up
    - Detailed code comments
    - Reorg of code

--- acoustics/FemModule.cc
+++ acoustics/FemModule.cc
@@ -34,10 +34,15 @@ using namespace Arcane;
 using namespace Arcane::FemUtils;
 
 /*---------------------------------------------------------------------------*/
-/*---------------------------------------------------------------------------*/
-/*!
- * \brief Module Fem.
+/**
+ * @brief A module for finite element method.
+ *
+ * This class handles the initialization and computation for finite element
+ * method (FEM) simulations, providing methods to  set  up and solve linear
+ * systems, assemble FEM operators, and perform result checks.
  */
+/*---------------------------------------------------------------------------*/
+
 class FemModule
 : public ArcaneFemObject
 {
@@ -52,8 +57,6 @@ class FemModule
     cm->setAllowUnkownRootElelement(false);
   }
 
- public:
-
   //! Method called at each iteration
   void compute() override;
 
@@ -67,25 +70,22 @@ class FemModule
 
  private:
 
-  Real kc2;
+  Real m_kc2;
 
   DoFLinearSystem m_linear_system;
-  IItemFamily* m_dof_family = nullptr;
+  IItemFamily* m_dof_family;
   FemDoFsOnNodes m_dofs_on_nodes;
-
- private:
 
   void _doStationarySolve();
   void _getMaterialParameters();
   void _assembleBilinearOperatorTRIA3();
   void _solve();
   void _assembleLinearOperator();
-  void _checkResultFile();
+  void _validateResults();
   FixedMatrix<3, 3> _computeElementMatrixTRIA3(Cell cell);
   Real _computeAreaTriangle3(Cell cell);
   Real _computeEdgeLength2(Face face);
   Real2 _computeEdgeNormal2(Face face);
-
 };
 
 /*---------------------------------------------------------------------------*/
@@ -103,15 +103,6 @@ compute()
   m_linear_system.reset();
   m_linear_system.setLinearSystemFactory(options()->linearSystem());
   m_linear_system.initialize(subDomain(), m_dofs_on_nodes.dofFamily(), "Solver");
-  // Test for adding parameters for PETSc.
-  // This is only used for the first call.
-  {
-    StringList string_list;
-    string_list.add("-trmalloc");
-    string_list.add("-log_trace");
-    CommandLineArguments args(string_list);
-    m_linear_system.setSolverCommandLineArguments(args);
-  }
   info() << "NB_CELL=" << allCells().size() << " NB_FACE=" << allFaces().size();
   _doStationarySolve();
 }
@@ -128,25 +119,26 @@ startInit()
 }
 
 /*---------------------------------------------------------------------------*/
+/**
+ * @brief Performs a stationary solve for the FEM system.
+ *
+ * This method follows a sequence of steps to solve FEM system:
+ *   1. _getMaterialParameters()          Retrieves material parameters via
+ *   2. _assembleBilinearOperatorTRIA3()  Assembles the FEM  matrix A
+ *   3. _assembleLinearOperator()         Assembles the FEM RHS vector b
+ *   4.  _solve()                         Solves for solution vector u = A^-1*b
+ *   5. _validateResults()                Regression test
+ */
 /*---------------------------------------------------------------------------*/
 
 void FemModule::
 _doStationarySolve()
 {
-  // get material parameters
   _getMaterialParameters();
-
-  // Assemble the FEM bilinear operator (LHS - matrix A)
   _assembleBilinearOperatorTRIA3();
-
-  // Assemble the FEM linear operator (RHS - vector b)
   _assembleLinearOperator();
-
-  // Solve the FEM linear system u= A^1*b
   _solve();
-
-  // Check results
-  _checkResultFile();
+  _validateResults();
 }
 
 /*---------------------------------------------------------------------------*/
@@ -156,96 +148,90 @@ void FemModule::
 _getMaterialParameters()
 {
   info() << "Get material parameters...";
-  kc2 = options()->kc2();
-
+  m_kc2 = options()->kc2();
 }
 
 /*---------------------------------------------------------------------------*/
-// Assemble the FEM linear operator
+/**
+ * @brief Assembles the FEM linear operator.
+ *
+ * This method assembles the RHS 'b' of the linear system by:
+ *   1. Initializing the RHS values to zero.
+ *   2. Iterating over Neumann boundary conditions to apply constant flux terms.
+ *      2.1 The integral of the flux term is computed over the boundary faces.
+ *      2.2 Handles both scalar and vector flux components.
+ *   3. For each boundary face, the edge length and normal are computed.
+ *   4. The contribution to the RHS is calculated and accumulated for each node.
+ */
 /*---------------------------------------------------------------------------*/
 
-void FemModule::
-_assembleLinearOperator()
+void FemModule::_assembleLinearOperator()
 {
   info() << "Assembly of FEM linear operator ";
 
-  // Temporary variable to keep values for the RHS part of the linear system
-  VariableDoFReal& rhs_values(m_linear_system.rhsVariable());
+  // step 1
+  VariableDoFReal& rhs_values(m_linear_system.rhsVariable()); // Temporary variable for RHS values
   rhs_values.fill(0.0);
 
   auto node_dof(m_dofs_on_nodes.nodeDoFConnectivityView());
 
-  //----------------------------------------------
-  // Constant flux term assembly
-  //----------------------------------------------
-  //
-  //  $int_{dOmega_N}((q.n)*v^h)$
-  // or
-  //  $int_{dOmega_N}((n_x*q_x + n_y*q_y)*v^h)$
-  //----------------------------------------------
+  // setp 2
   for (const auto& bs : options()->neumannBoundaryCondition()) {
     FaceGroup group = bs->surface();
 
-    if(bs->value.isPresent()) {
-      Real value = bs->value();
-      ENUMERATE_ (Face, iface, group) {
-        Face face = *iface;
-        Real length = _computeEdgeLength2(face);
-        for (Node node : iface->nodes()) {
-          if (node.isOwn())
-            rhs_values[node_dof.dofId(node, 0)] += value * length / 2.;
-        }
-      }
-      continue;
+    Real value = 0.0;
+    Real valueX = 0.0;
+    Real valueY = 0.0;
+    bool hasValue = bs->value.isPresent();
+    bool hasValueX = bs->valueX.isPresent();
+    bool hasValueY = bs->valueY.isPresent();
+
+    if (hasValue) {
+      value = bs->value();
+    }
+    else {
+      if (hasValueX)
+        valueX = bs->valueX();
+      if (hasValueY)
+        valueY = bs->valueY();
     }
 
+    // setp 2.1
+    ENUMERATE_ (Face, iface, group) {
+      Face face = *iface;
 
-    if(bs->valueX.isPresent()  && bs->valueY.isPresent()) {
-      Real valueX = bs->valueX();
-      Real valueY = bs->valueY();
-      ENUMERATE_ (Face, iface, group) {
-        Face face = *iface;
-        Real  length = _computeEdgeLength2(face);
-        Real2 Normal = _computeEdgeNormal2(face);
-        for (Node node : iface->nodes()) {
-          if (node.isOwn())
-            rhs_values[node_dof.dofId(node, 0)] += (Normal.x*valueX + Normal.y*valueY) * length / 2.;
-        }
-      }
-      continue;
-    }
+      // step 3.
+      Real length = _computeEdgeLength2(face);
+      Real2 normal = _computeEdgeNormal2(face);
 
-    if(bs->valueX.isPresent()) {
-      Real valueX = bs->valueX();
-      ENUMERATE_ (Face, iface, group) {
-        Face face = *iface;
-        Real  length = _computeEdgeLength2(face);
-        Real2 Normal = _computeEdgeNormal2(face);
-        for (Node node : iface->nodes()) {
-          if (node.isOwn())
-            rhs_values[node_dof.dofId(node, 0)] += (Normal.x*valueX) * length / 2.;
-        }
-      }
-      continue;
-    }
+      // step 4
+      for (Node node : iface->nodes()) {
+        if (!node.isOwn())
+          continue;
+        Real rhs_value = 0.0;
 
-    if(bs->valueY.isPresent()) {
-      Real valueY = bs->valueY();
-      ENUMERATE_ (Face, iface, group) {
-        Face face = *iface;
-        Real  length = _computeEdgeLength2(face);
-        Real2 Normal = _computeEdgeNormal2(face);
-        for (Node node : iface->nodes()) {
-          if (node.isOwn())
-            rhs_values[node_dof.dofId(node, 0)] += (Normal.y*valueY) * length / 2.;
+        // step 2.2
+        if (hasValue) {
+          rhs_value = value * length / 2.0;
         }
+        else {
+          rhs_value = (normal.x * valueX + normal.y * valueY) * length / 2.0;
+        }
+
+        rhs_values[node_dof.dofId(node, 0)] += rhs_value;
       }
-      continue;
     }
   }
 }
 
 /*---------------------------------------------------------------------------*/
+/**
+ * @brief Computes the area of a triangle defined by three nodes.
+ *
+ * This method calculates the area using the determinant formula for a triangle.
+ * The area is computed as half the value of the determinant of the matrix
+ * formed by the coordinates of the triangle's vertices.
+ */
 /*---------------------------------------------------------------------------*/
 
 Real FemModule::
@@ -258,68 +244,92 @@ _computeAreaTriangle3(Cell cell)
 }
 
 /*---------------------------------------------------------------------------*/
+/**
+ * @brief Computes the length of the edge defined by a given face.
+ * 
+ * This method calculates Euclidean distance between the two nodes of the face.
+ */
 /*---------------------------------------------------------------------------*/
-
 Real FemModule::
 _computeEdgeLength2(Face face)
 {
   Real3 m0 = m_node_coord[face.nodeId(0)];
   Real3 m1 = m_node_coord[face.nodeId(1)];
-  return  math::sqrt((m1.x-m0.x)*(m1.x-m0.x) + (m1.y-m0.y)*(m1.y - m0.y));
+
+  Real dx = m1.x - m0.x;
+  Real dy = m1.y - m0.y;
+
+  return math::sqrt(dx * dx + dy * dy);
 }
 
 /*---------------------------------------------------------------------------*/
+/**
+ * @brief Computes the normalized edge normal for a given face.
+ *
+ * This method calculates normal vector to the edge defined by nodes of the face,
+ * normalizes it, and ensures the correct orientation.
+ */
 /*---------------------------------------------------------------------------*/
-
 Real2 FemModule::
 _computeEdgeNormal2(Face face)
 {
   Real3 m0 = m_node_coord[face.nodeId(0)];
   Real3 m1 = m_node_coord[face.nodeId(1)];
+
   if (!face.isSubDomainBoundaryOutside())
-    std::swap(m0,m1);
-  Real2 N;
-  Real norm_N = math::sqrt( (m1.y - m0.y)*(m1.y - m0.y) + (m1.x - m0.x)*(m1.x - m0.x) );   // for normalizing
-  N.x = (m1.y - m0.y)/ norm_N;
-  N.y = (m0.x - m1.x)/ norm_N;
-  return  N;
+    std::swap(m0, m1);
+
+  Real dx = m1.x - m0.x;
+  Real dy = m1.y - m0.y;
+  Real norm_N = math::sqrt(dx * dx + dy * dy);
+
+  return { dy / norm_N, -dx / norm_N };
 }
 
 /*---------------------------------------------------------------------------*/
-// Compute the element matrix for a triangular element (P1 finite elements)
-// - This function calculates the integral of ( -(u.dx*v.dx + u.dy*v.dy) + sigma*u*v)
-//   over the triangular element using linear shape functions (P1)
+/**
+ * @brief Computes the element matrix for a triangular element (P1 FE).
+ *
+ * This function calculates the integral of the expression:
+ * -(u.dx * v.dx + u.dy * v.dy) + kc2 * u * v
+ *
+ * Steps involved:
+ * 1. Get the coordinates of the triangle vertices.
+ * 2. Calculate the area of the triangle.
+ * 3. Compute the gradients of the shape functions.
+ * 4. Normalize the gradients by the area.
+ * 5. Compute the element matrix:
+ *    5.1 first compute -(u.dx * v.dx + u.dy * v.dy) terms
+ *    5.2 then (kc2 * u * v) term adjusted
+ */
 /*---------------------------------------------------------------------------*/
 
 FixedMatrix<3, 3> FemModule::
 _computeElementMatrixTRIA3(Cell cell)
 {
-  // Get coordinates of the triangle vertices
+  // step 1
   Real3 vertex0 = m_node_coord[cell.nodeId(0)];
   Real3 vertex1 = m_node_coord[cell.nodeId(1)];
   Real3 vertex2 = m_node_coord[cell.nodeId(2)];
 
-  // Calculate the area of the triangle
+  // step 2
   Real area = _computeAreaTriangle3(cell);
 
-  // Compute the gradients of the shape functions
+  // step 3
   Real2 gradN0(vertex1.y - vertex2.y, vertex2.x - vertex1.x);
   Real2 gradN1(vertex2.y - vertex0.y, vertex0.x - vertex2.x);
   Real2 gradN2(vertex0.y - vertex1.y, vertex1.x - vertex0.x);
 
-  // Normalize the gradients by the area
+  // step 4
   Real A2 = 2.0 * area;
   gradN0 /= A2;
   gradN1 /= A2;
   gradN2 /= A2;
 
-
-  // Compute the element matrix
+  // step 5
   FixedMatrix<3, 3> elementMatrix;
 
-  // -(u.dx*v.dx + u.dy*v.dy) terms
-  // For P1 elements, dx and dy are symmetric in the integral evaluation
-  // So these terms are the same as dx(u) * dx(v) terms
+  // step 5.1
   elementMatrix(0, 0) = -area * Arcane::math::dot(gradN0, gradN0);
   elementMatrix(0, 1) = -area * Arcane::math::dot(gradN0, gradN1);
   elementMatrix(0, 2) = -area * Arcane::math::dot(gradN0, gradN2);
@@ -332,27 +342,31 @@ _computeElementMatrixTRIA3(Cell cell)
   elementMatrix(2, 1) = -area * Arcane::math::dot(gradN2, gradN1);
   elementMatrix(2, 2) = -area * Arcane::math::dot(gradN2, gradN2);
 
-  // Subtract uv term
-  // For P1 elements, the integral of uv over the element is:
-  // Diagonal terms: area / 6
-  // Off-diagonal terms: area / 12
-  Real uvTerm = kc2*area / 12.0;
-  elementMatrix(0, 0) += uvTerm*2.;
+  // step 5 .2
+  Real uvTerm = m_kc2 * area / 12.0;
+  elementMatrix(0, 0) += uvTerm * 2.;
   elementMatrix(0, 1) += uvTerm;
   elementMatrix(0, 2) += uvTerm;
 
   elementMatrix(1, 0) += uvTerm;
-  elementMatrix(1, 1) += uvTerm*2.;
+  elementMatrix(1, 1) += uvTerm * 2.;
   elementMatrix(1, 2) += uvTerm;
 
   elementMatrix(2, 0) += uvTerm;
   elementMatrix(2, 1) += uvTerm;
-  elementMatrix(2, 2) += uvTerm*2.;
+  elementMatrix(2, 2) += uvTerm * 2.;
 
   return elementMatrix;
 }
 
 /*---------------------------------------------------------------------------*/
+/**
+ * @brief Assembles the bilinear operator matrix for triangular elements.
+ *
+ * This method computes and assembles the global  matrix A for the FEM linear
+ * system. For each cell (triangle), it calculates the local element matrix &
+ * adds the contributions to the global matrix based on the nodes of the cell.
+ */
 /*---------------------------------------------------------------------------*/
 
 void FemModule::
@@ -362,11 +376,8 @@ _assembleBilinearOperatorTRIA3()
 
   ENUMERATE_ (Cell, icell, allCells()) {
     Cell cell = *icell;
-    if (cell.type() != IT_Triangle3)
-      ARCANE_FATAL("Only Triangle3 cell type is supported");
 
-    // element stiffness matrix
-    auto K_e = _computeElementMatrixTRIA3(cell);
+    auto K_e = _computeElementMatrixTRIA3(cell); // element matrix
     Int32 n1_index = 0;
     for (Node node1 : cell.nodes()) {
       Int32 n2_index = 0;
@@ -383,50 +394,66 @@ _assembleBilinearOperatorTRIA3()
 }
 
 /*---------------------------------------------------------------------------*/
+/**
+ * @brief Solves the linear system and updates the solution vector.
+ *
+ * This method performs the following actions:
+ *   1. Solves the linear system to compute the solution.
+ *   2. Copies the computed solution from the DoF to the node values.
+ *   3. Synchronizes the updated node values.
+ */
 /*---------------------------------------------------------------------------*/
 
 void FemModule::
 _solve()
 {
+  // step 1
   m_linear_system.solve();
 
-  {
-    VariableDoFReal& dof_u(m_linear_system.solutionVariable());
-    // Copy RHS DoF to Node u
-    auto node_dof(m_dofs_on_nodes.nodeDoFConnectivityView());
-    ENUMERATE_ (Node, inode, ownNodes()) {
-      Node node = *inode;
-      Real v = dof_u[node_dof.dofId(node, 0)];
-      m_u[node] = v;
-    }
+  // step 2
+  VariableDoFReal& dof_u(m_linear_system.solutionVariable());
+  auto node_dof(m_dofs_on_nodes.nodeDoFConnectivityView());
+
+  ENUMERATE_ (Node, inode, ownNodes()) {
+    Node node = *inode;
+    m_u[node] = dof_u[node_dof.dofId(node, 0)];
   }
 
+  // step 3
   m_u.synchronize();
-
-  const bool do_print = (allNodes().size() < 200);
-  if (do_print) {
-    ENUMERATE_ (Node, inode, allNodes()) {
-      Node node = *inode;
-      info() << "u[" << node.localId() << "][" << node.uniqueId() << "] = "
-             << m_u[node];
-      //info() << "u[]" << node.uniqueId() << " "
-      //       << m_u[node];
-    }
-  }
 }
 
 /*---------------------------------------------------------------------------*/
+/**
+ * @brief Validates and prints the results of the FEM computation.
+ * 
+ * This method performs the following actions:
+ *   1. If number of nodes < 200, prints the computed values for each node.
+ *   2. Retrieves the filename for the result file from options.
+ *   3. If a filename is provided, checks the computed results against result file.
+ * 
+ * @note The result comparison uses a tolerance of 1.0e-4.
+ */
 /*---------------------------------------------------------------------------*/
-
 void FemModule::
-_checkResultFile()
+_validateResults()
 {
+
+  // setp 1
+  if (allNodes().size() < 200) {
+    ENUMERATE_ (Node, inode, allNodes()) {
+      Node node = *inode;
+      info() << "u[" << node.localId() << "][" << node.uniqueId() << "] = " << m_u[node];
+    }
+  }
+
+  // setp 2
   String filename = options()->resultFile();
-  info() << "CheckResultFile filename=" << filename;
-  if (filename.empty())
-    return;
-  const double epsilon = 1.0e-4;
-  checkNodeResultFile(traceMng(), filename, m_u, epsilon);
+  info() << "ValidateResultFile filename=" << filename;
+
+  // setp 3
+  if (!filename.empty())
+    checkNodeResultFile(traceMng(), filename, m_u, 1.0e-4);
 }
 
 /*---------------------------------------------------------------------------*/
--- acoustics/Test.sub.arc
+++ acoustics/Test.sub.arc
@@ -1,23 +1,43 @@
 <?xml version="1.0"?>
+<!--
+  Case configuration for an acoustics simulation. 
+  The following sections define:
+    - General simulation settings
+    - Mesh configurations details
+    - Finite Element Method (FEM) configurations
+    - Post-processing options
+-->
 <case codename="Acoustics" xml:lang="en" codeversion="1.0">
+
+  <!--
+    Arcane-specific settings:
+      - title: The name or description of the case.
+      - timeloop: Defines the time-stepping loop for the simulation.
+  -->
   <arcane>
-    <title>Sample</title>
+    <title>Submarine Toy Case</title>
     <timeloop>AcousticsLoop</timeloop>
   </arcane>
 
-  <arcane-post-processing>
-   <output-period>1</output-period>
-   <output>
-     <variable>U</variable>
-   </output>
-  </arcane-post-processing>
 
+  <!--
+    Mesh configurations:
+      - filename: Path to the mesh file used in the simulation.
+  -->
   <meshes>
     <mesh>
       <filename>sub.msh</filename>
     </mesh>
   </meshes>
 
+
+  <!--
+    FEM (Finite Element Method) settings:
+      - kc2: Coefficient used in the FEM calculations.
+      - neumann-boundary-condition: Defines boundary conditions for the simulation.
+      - linear-system: Specifies the linear system solver to use.
+      - result-file: File for validation (optional)
+  -->
   <fem>
     <kc2>.11e1</kc2>
     <neumann-boundary-condition>
@@ -25,7 +45,19 @@
       <value>1.0</value>
     </neumann-boundary-condition>
     <linear-system name="SequentialBasicLinearSystem" />
-
     <result-file>sub_2D.txt</result-file>
   </fem>
+
+  <!--
+    Post-processing settings:
+      - output-period: Defines how often output should be generated.
+      - output: Specifies which variables are to be outputted.
+  -->
+  <arcane-post-processing>
+   <output-period>1</output-period>
+   <output>
+     <variable>U</variable>
+   </output>
+  </arcane-post-processing>
+
 </case>
