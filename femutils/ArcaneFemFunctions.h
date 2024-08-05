78ba56818d5696493fbc91964ea8d35d13b7cd3e
Mohd Afeef Badri <52162083+mohd-afeef-badri@users.noreply.github.com>
2024-08-05 16:36:21 +0200

    calculate full graident dx and dy

--- femutils/ArcaneFemFunctions.h
+++ femutils/ArcaneFemFunctions.h
@@ -75,9 +75,19 @@ class ArcaneFemFunctions
   /**
    * @brief Computes the gradients of basis functions N for P1 triangles.
    *
-   * This method calculates gradient operator for a given P1 cell
+   * This method calculates gradient operator ∇ Ni for a given P1 cell
+   * with i = 1,..,3 for the three shape function Ni hence output is a
+   * matrix of size 3x3
    *
-   * @note we are using 3x3 matrix for this even in 2D hence the z gradient is 0.
+   *              [ ∂N1/∂x     ∂N1/∂y   ∂N1/∂z  ]
+   *         ∇N = [ ∂N2/∂x     ∂N2/∂y   ∂N2/∂z  ]
+   *              [ ∂N3/∂x     ∂N3/∂y   ∂N3/∂z  ]
+   *
+   *                     [ y2​−y3     x3​−x2    0  ]
+   *         ∇N = 1/(2A) [ y3−y1     x1−x3    0  ]
+   *                     [ y1−y2     x2−x1    0  ]
+   *
+   * @note we can adapt the same for 3D by filling the third component
    */
   /*---------------------------------------------------------------------------*/
 
@@ -87,9 +97,11 @@ class ArcaneFemFunctions
     Real3 vertex1 = node_coord[cell.nodeId(1)];
     Real3 vertex2 = node_coord[cell.nodeId(2)];
 
-    return Real3x3(Real3(vertex1.y - vertex2.y, vertex2.x - vertex1.x, 0),
-                   Real3(vertex2.y - vertex0.y, vertex0.x - vertex2.x, 0),
-                   Real3(vertex0.y - vertex1.y, vertex1.x - vertex0.x, 0));
+    Real A2 = ((vertex1.x - vertex0.x) * (vertex2.y - vertex0.y) - (vertex2.x - vertex0.x) * (vertex1.y - vertex0.y));
+
+    return Real3x3(Real3((vertex1.y - vertex2.y)/A2, (vertex2.x - vertex1.x)/A2, 0),
+                   Real3((vertex2.y - vertex0.y)/A2, (vertex0.x - vertex2.x)/A2, 0),
+                   Real3((vertex0.y - vertex1.y)/A2, (vertex1.x - vertex0.x)/A2, 0));
   }
 };
 
