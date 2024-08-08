993806852e9134495ee632ecf680bb617b72bb61
Mohd Afeef Badri <52162083+mohd-afeef-badri@users.noreply.github.com>
2024-08-08 15:03:57 +0200

    new functions For Bary Center calculation For Dirichlet BC

--- femutils/ArcaneFemFunctions.h
+++ femutils/ArcaneFemFunctions.h
@@ -41,7 +41,7 @@ class ArcaneFemFunctions
      * formed by the coordinates of the triangle's vertices.
      */
     /*---------------------------------------------------------------------------*/
-  
+
     static inline Real computeAreaTriangle3(Cell cell, const VariableNodeReal3& node_coord)
     {
       Real3 vertex0 = node_coord[cell.nodeId(0)];
@@ -49,6 +49,31 @@ class ArcaneFemFunctions
       Real3 vertex2 = node_coord[cell.nodeId(2)];
 
       return 0.5 * ((vertex1.x - vertex0.x) * (vertex2.y - vertex0.y) - (vertex2.x - vertex0.x) * (vertex1.y - vertex0.y));
+    }
+
+    /*---------------------------------------------------------------------------*/
+    /**
+     * @brief Computes the barycenter (centroid) of a triangle.
+     *
+     * This method calculates the barycenter of a triangle defined by three nodes.
+     * The barycenter is computed as the average of the vertices' coordinates.
+     *
+     * @param cell The triangle cell.
+     * @param node_coord The coordinates of the nodes.
+     * @return The 2D barycenter coordinates as a `Real3` object.
+     */
+    /*---------------------------------------------------------------------------*/
+
+    static inline Real3 computeBaryCenterTriangle3(Cell cell, const VariableNodeReal3& node_coord)
+    {
+      Real3 vertex0 = node_coord[cell.nodeId(0)];
+      Real3 vertex1 = node_coord[cell.nodeId(1)];
+      Real3 vertex2 = node_coord[cell.nodeId(2)];
+
+      Real Center_x = (vertex0.x + vertex1.x + vertex2.x) / 3.;
+      Real Center_y = (vertex0.y + vertex1.y + vertex2.y) / 3.;
+
+      return { Center_x, Center_y, 0 };
     }
 
     /*---------------------------------------------------------------------------*/
@@ -72,11 +97,11 @@ class ArcaneFemFunctions
 
     /*---------------------------------------------------------------------------*/
     /**
-   * @brief Computes the normalized edge normal for a given face.
-   *
-   * This method calculates normal vector to the edge defined by nodes of the face,
-   * normalizes it, and ensures the correct orientation.
-   */
+     * @brief Computes the normalized edge normal for a given face.
+     *
+     * This method calculates normal vector to the edge defined by nodes of the face,
+     * normalizes it, and ensures the correct orientation.
+     */
     /*---------------------------------------------------------------------------*/
 
     static inline Real2 computeEdgeNormal2(Face face, const VariableNodeReal3& node_coord)
@@ -97,11 +122,11 @@ class ArcaneFemFunctions
 
   /*---------------------------------------------------------------------------*/
   /**
- * @brief Provides methods for finite element operations in 2D.
- *
- * This class includes static methods for calculating gradients of basis
- * functions and integrals for P1 triangles in 2D finite element analysis.
- */
+  * @brief Provides methods for finite element operations in 2D.
+  *
+  * This class includes static methods for calculating gradients of basis
+  * functions and integrals for P1 triangles in 2D finite element analysis.
+  */
   /*---------------------------------------------------------------------------*/
   class FeOperation2D
   {
@@ -110,22 +135,22 @@ class ArcaneFemFunctions
 
     /*---------------------------------------------------------------------------*/
     /**
-   * @brief Computes the gradients of basis functions N for P1 triangles.
-   *
-   * This method calculates gradient operator ∇ Ni for a given P1 cell
-   * with i = 1,..,3 for the three shape function Ni hence output is a
-   * matrix of size 3x3
-   *
-   *              [ ∂N1/∂x     ∂N1/∂y   ∂N1/∂z  ]
-   *         ∇N = [ ∂N2/∂x     ∂N2/∂y   ∂N2/∂z  ]
-   *              [ ∂N3/∂x     ∂N3/∂y   ∂N3/∂z  ]
-   *
-   *                     [ y2​−y3     x3​−x2    0  ]
-   *         ∇N = 1/(2A) [ y3−y1     x1−x3    0  ]
-   *                     [ y1−y2     x2−x1    0  ]
-   *
-   * @note we can adapt the same for 3D by filling the third component
-   */
+     * @brief Computes the gradients of basis functions N for P1 triangles.
+     *
+     * This method calculates gradient operator ∇ Ni for a given P1 cell
+     * with i = 1,..,3 for the three shape function Ni hence output is a
+     * matrix of size 3x3
+     *
+     *              [ ∂N1/∂x     ∂N1/∂y   ∂N1/∂z  ]
+     *         ∇N = [ ∂N2/∂x     ∂N2/∂y   ∂N2/∂z  ]
+     *              [ ∂N3/∂x     ∂N3/∂y   ∂N3/∂z  ]
+     *
+     *                     [ y2​−y3     x3​−x2    0  ]
+     *         ∇N = 1/(2A) [ y3−y1     x1−x3    0  ]
+     *                     [ y1−y2     x2−x1    0  ]
+     *
+     * @note we can adapt the same for 3D by filling the third component
+     */
     /*---------------------------------------------------------------------------*/
 
     static inline Real3x3 computeGradientTria3(Cell cell, const VariableNodeReal3& node_coord)
@@ -143,16 +168,16 @@ class ArcaneFemFunctions
 
     /*---------------------------------------------------------------------------*/
     /**
-   * @brief Computes the X gradients of basis functions N for P1 triangles.
-   *
-   * This method calculates gradient operator ∂/∂x of Ni for a given P1
-   * cell with i = 1,..,3 for the three shape function  Ni  hence output
-   * is a vector of size 3
-   *
-   *         ∂N/∂x = [ ∂N1/∂x  ∂N1/∂x  ∂N3/∂x ]
-   *
-   *         ∂N/∂x = 1/(2A) [ y2​−y3  y3−y1  y1−y2 ]
-   */
+     * @brief Computes the X gradients of basis functions N for P1 triangles.
+     *
+     * This method calculates gradient operator ∂/∂x of Ni for a given P1
+     * cell with i = 1,..,3 for the three shape function  Ni  hence output
+     * is a vector of size 3
+     *
+     *         ∂N/∂x = [ ∂N1/∂x  ∂N1/∂x  ∂N3/∂x ]
+     *
+     *         ∂N/∂x = 1/(2A) [ y2​−y3  y3−y1  y1−y2 ]
+     */
     /*---------------------------------------------------------------------------*/
 
     static inline Real3 computeGradientXTria3(Cell cell, const VariableNodeReal3& node_coord)
@@ -168,16 +193,16 @@ class ArcaneFemFunctions
 
     /*---------------------------------------------------------------------------*/
     /**
-   * @brief Computes the Y gradients of basis functions N for P1 triangles.
-   *
-   * This method calculates gradient operator ∂/∂y of Ni for a given P1
-   * cell with i = 1,..,3 for the three shape function  Ni  hence output
-   * is a vector of size 3
-   *
-   *         ∂N/∂x = [ ∂N1/∂y  ∂N1/∂y  ∂N3/∂y ]
-   *
-   *         ∂N/∂x = 1/(2A) [ x3​−x2  x1−x3  x2−x1 ]
-   */
+     * @brief Computes the Y gradients of basis functions N for P1 triangles.
+     *
+     * This method calculates gradient operator ∂/∂y of Ni for a given P1
+     * cell with i = 1,..,3 for the three shape function  Ni  hence output
+     * is a vector of size 3
+     *
+     *         ∂N/∂x = [ ∂N1/∂y  ∂N1/∂y  ∂N3/∂y ]
+     *
+     *         ∂N/∂x = 1/(2A) [ x3​−x2  x1−x3  x2−x1 ]
+     */
     /*---------------------------------------------------------------------------*/
 
     static inline Real3 computeGradientYTria3(Cell cell, const VariableNodeReal3& node_coord)
@@ -193,15 +218,15 @@ class ArcaneFemFunctions
 
     /*---------------------------------------------------------------------------*/
     /**
-   * @brief Computes the integral (u*v) for P1 triangles.
-   *
-   * here the element matrix will read
-   *
-   *              [ 1/6     1/12   1/12 ]
-   *         a  = [ 1/12    1/6    1/12 ]
-   *              [ 1/12    1/12   1/6  ]
-   *
-   */
+     * @brief Computes the integral (u*v) for P1 triangles.
+     *
+     * here the element matrix will read
+     *
+     *              [ 1/6     1/12   1/12 ]
+     *         a  = [ 1/12    1/6    1/12 ]
+     *              [ 1/12    1/12   1/6  ]
+     *
+     */
     /*---------------------------------------------------------------------------*/
 
     static inline Real3x3 computeUVTria3(Cell cell, const VariableNodeReal3& node_coord)
@@ -214,11 +239,11 @@ class ArcaneFemFunctions
 
   /*---------------------------------------------------------------------------*/
   /**
- * @brief Provides methods for applying boundary conditions in 2D FEM problems.
- *
- * This class includes static methods for applying Neumann boundary conditions
- * to the right-hand side (RHS) of finite element method equations in 2D.
- */
+   * @brief Provides methods for applying boundary conditions in 2D FEM problems.
+   *
+   * This class includes static methods for applying Neumann boundary conditions
+   * to the right-hand side (RHS) of finite element method equations in 2D.
+   */
   /*---------------------------------------------------------------------------*/
   class BoundaryConditions2D
   {
@@ -226,17 +251,17 @@ class ArcaneFemFunctions
 
     /*---------------------------------------------------------------------------*/
     /**
- * @brief Applies Neumann conditions to the right-hand side (RHS) values.
- *
- * This method updates the RHS values of the finite element method equations
- * based on the provided Neumann boundary condition. The boundary condition
- * can specify a value or its components along the x and y directions.
- *
- * @param [IN]  bs         : The Neumann boundary condition values.
- * @param [IN]  node_dof   : Connectivity view for degrees of freedom at nodes.
- * @param [IN]  node_coord : Coordinates of the nodes in the mesh.
- * @param [OUT] rhs_values : The right-hand side values to be updated.
- */
+     * @brief Applies Neumann conditions to the right-hand side (RHS) values.
+     *
+     * This method updates the RHS values of the finite element method equations
+     * based on the provided Neumann boundary condition. The boundary condition
+     * can specify a value or its components along the x and y directions.
+     *
+     * @param [IN]  bs         : The Neumann boundary condition values.
+     * @param [IN]  node_dof   : Connectivity view for degrees of freedom at nodes.
+     * @param [IN]  node_coord : Coordinates of the nodes in the mesh.
+     * @param [OUT] rhs_values : The right-hand side values to be updated.
+     */
     /*---------------------------------------------------------------------------*/
 
     static inline void applyNeumannToRhs(const CaseOptionsFem::CaseOptionNeumannBoundaryConditionValue* bs, const Arcane::IndexedNodeDoFConnectivityView& node_dof, const Arcane::VariableNodeReal3& node_coord, Arcane::VariableDoFReal& rhs_values)
@@ -279,6 +304,39 @@ class ArcaneFemFunctions
           }
 
           rhs_values[node_dof.dofId(node, 0)] += rhs_value;
+        }
+      }
+    }
+
+    /*---------------------------------------------------------------------------*/
+    /**
+     * @brief Applies Dirichlet boundary conditions to RHS and LHS.
+     *
+     * Updates the LHS matrix and RHS vector to enforce Dirichlet conditions.
+     * 
+     * - For LHS matrix `A`, the diagonal term for the Dirichlet DOF is set to `P`.
+     * - For RHS vector `b`, the Dirichlet DOF term is scaled by `P`.
+     *
+     * @param [IN]  bs              : Boundary condition values.
+     * @param [IN]  node_dof        : DOF connectivity view.
+     * @param [IN]  node_coord      : Node coordinates.
+     * @param [OUT] m_linear_system : Linear system for LHS.
+     * @param [OUT] rhs_values RHS  : RHS values to update.
+     */
+    /*---------------------------------------------------------------------------*/
+    static inline void applyDirichletToLhsAndRhs(const CaseOptionsFem::CaseOptionDirichletBoundaryConditionValue* bs, const Arcane::IndexedNodeDoFConnectivityView& node_dof, const Arcane::VariableNodeReal3& node_coord, FemUtils::DoFLinearSystem& m_linear_system, Arcane::VariableDoFReal& rhs_values)
+    {
+      FaceGroup group = bs->surface();
+      Real value = bs->value();
+      Real Penalty = bs->penalty();
+
+      ENUMERATE_ (Face, iface, group) {
+        for (Node node : iface->nodes()) {
+          if (node.isOwn()) {
+            m_linear_system.matrixSetValue(node_dof.dofId(node, 0), node_dof.dofId(node, 0), Penalty);
+            Real u_g = Penalty * value;
+            rhs_values[node_dof.dofId(node, 0)] = u_g;
+          }
         }
       }
     }
