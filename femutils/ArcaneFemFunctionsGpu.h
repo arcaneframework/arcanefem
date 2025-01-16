d7ac5d84a0a3ddcf548f60f7ac0662cfc1199d79
Charles-Antoine Leger <ca@leger.email>
2025-01-16 11:38:15 +0100

    feat[femutils]: Neumann BC (2D, scalar) on GPU

--- femutils/ArcaneFemFunctionsGpu.h
+++ femutils/ArcaneFemFunctionsGpu.h
@@ -15,16 +15,32 @@
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 
-#include <arcane/accelerator/VariableViews.h>
-#include <arccore/base/ArccoreGlobal.h>
-#include <arcane/utils/ArcaneGlobal.h>
+#include <arcane/VariableTypes.h>
+#include <arcane/IMesh.h>
 
+#include <arcane/accelerator/RunCommandEnumerate.h>
+#include <arcane/accelerator/AcceleratorGlobal.h>
+#include <arcane/accelerator/NumArrayViews.h>
+#include <arcane/accelerator/VariableViews.h>
+#include <arcane/accelerator/RunCommand.h>
+#include <arcane/accelerator/RunQueue.h>
+#include <arcane/accelerator/Atomic.h>
+
+#include <arccore/base/ArccoreGlobal.h>
+
+#include <arcane/core/UnstructuredMeshConnectivity.h>
 #include <arcane/core/IndexedItemConnectivityView.h>
+#include <arcane/core/ItemInfoListView.h>
 #include <arcane/core/MathUtils.h>
+#include <arcane/core/ItemTypes.h>
 #include <arcane/core/Item.h>
 
-#include "FemUtils.h"
+#include <arcane/utils/ArcaneGlobal.h>
+#include <arcane/utils/NumArray.h>
+
+#include "FemDoFsOnNodes.h"
 #include "IArcaneFemBC.h"
+#include "FemUtils.h"
 
 namespace Arcane::FemUtils::Gpu::MeshOperation
 {
@@ -73,6 +89,49 @@ ARCCORE_HOST_DEVICE static inline Real computeVolumeTetra4(CellLocalId cell_lid,
 
   return math::abs(math::dot(v0, math::cross(v1, v2))) / 6.0;
 };
+
+/*---------------------------------------------------------------------------*/
+/**
+ * @brief Computes the length of the edge defined by a given face.
+ *
+ * This method calculates Euclidean distance between the two nodes of the face.
+ */
+/*---------------------------------------------------------------------------*/
+
+ARCCORE_HOST_DEVICE static inline Real computeLengthFace(FaceLocalId face_lid, IndexedFaceNodeConnectivityView fn_cv, Accelerator::VariableNodeReal3InView in_node_coord)
+{
+
+  Real3 m0 = in_node_coord[fn_cv.nodeId(face_lid, 0)];
+  Real3 m1 = in_node_coord[fn_cv.nodeId(face_lid, 1)];
+  return math::sqrt((m1.x - m0.x) * (m1.x - m0.x) + (m1.y - m0.y) * (m1.y - m0.y));
+}
+
+/*---------------------------------------------------------------------------*/
+/**
+ * @brief Computes the normalized edge normal for a given face.
+ *
+ * This method calculates normal vector to the edge defined by nodes of the face,
+ * normalizes it, and ensures the correct orientation.
+ */
+/*---------------------------------------------------------------------------*/
+
+ARCCORE_HOST_DEVICE static inline Real2 computeNormalFace(FaceLocalId face_lid, IndexedFaceNodeConnectivityView fn_cv, Accelerator::VariableNodeReal3InView in_node_coord, Arcane::FaceInfoListView faces_infos)
+{
+  Real3 m0 = in_node_coord[fn_cv.nodeId(face_lid, 0)];
+  Real3 m1 = in_node_coord[fn_cv.nodeId(face_lid, 1)];
+
+  if (!faces_infos.isSubDomainBoundaryOutside(face_lid)) {
+    Real3 tmp = m0;
+    m0 = m1;
+    m1 = tmp;
+  }
+
+  Real2 N;
+  Real norm_N = math::sqrt((m1.y - m0.y) * (m1.y - m0.y) + (m1.x - m0.x) * (m1.x - m0.x));
+  N.x = (m1.y - m0.y) / norm_N;
+  N.y = (m0.x - m1.x) / norm_N;
+  return N;
+}
 
 } // namespace Arcane::FemUtils::Gpu::MeshOperation
 
@@ -274,7 +333,74 @@ ARCCORE_HOST_DEVICE static inline Real4 computeGradientZTetra4(CellLocalId cell_
 namespace Arcane::FemUtils::Gpu::BoundaryConditions2D
 {
 
-static inline void applyNeumannToRhs(BC::INeumannBoundaryCondition* bs, const IndexedNodeDoFConnectivityView& node_dof, const VariableNodeReal3& node_coord, VariableDoFReal& rhs_values) {}
+/*---------------------------------------------------------------------------*/
+/**
+ * @brief Applies Neumann conditions to the right-hand side (RHS) values.
+ *
+ * This method updates the RHS values of the finite element method equations
+ * based on the provided Neumann boundary condition. The boundary condition
+ * can specify a value or its components along the x and y directions.
+ *
+ */
+/*---------------------------------------------------------------------------*/
+
+static inline void applyNeumannToRhs(BC::INeumannBoundaryCondition* bs, Accelerator::RunQueue* queue, NumArray<Real, MDDim1>& rhs_variable_na, IMesh* mesh, const FemDoFsOnNodes& dofs_on_nodes, VariableNodeReal3 node_coord)
+{
+  ARCANE_CHECK_PTR(bs);
+  ARCANE_CHECK_PTR(queue);
+  ARCANE_CHECK_PTR(mesh);
+
+  FaceGroup group = bs->getSurface();
+  bool has_value = bs->hasValue();
+
+  UnstructuredMeshConnectivityView connectivity_view;
+  connectivity_view.setMesh(mesh);
+  NodeInfoListView nodes_infos(mesh->nodeFamily());
+  auto node_dof(dofs_on_nodes.nodeDoFConnectivityView());
+  auto fn_cv = connectivity_view.faceNode();
+
+  if (has_value) {
+    {
+      Real value = has_value ? bs->getValue() : 0.;
+
+      auto command = Accelerator::makeCommand(queue);
+      auto in_out_rhs_variable_na = Accelerator::viewInOut(command, rhs_variable_na);
+      auto in_node_coord = Accelerator::viewIn(command, node_coord);
+      command << RUNCOMMAND_ENUMERATE(FaceLocalId, face_lid, group)
+      {
+        Real length = MeshOperation::computeLengthFace(face_lid, fn_cv, in_node_coord);
+        for (NodeLocalId node_lid : fn_cv.nodes(face_lid)) {
+          if (nodes_infos.isOwn(node_lid)) {
+            Real rhs_value = value * length / 2.;
+            Accelerator::doAtomic<Accelerator::eAtomicOperation::Add>(in_out_rhs_variable_na[node_dof.dofId(node_lid, 0)], rhs_value);
+          }
+        }
+      };
+    }
+  }
+  else {
+    {
+      Real value_x = bs->hasValueX() ? bs->getValueX() : 0.;
+      Real value_y = bs->hasValueY() ? bs->getValueY() : 0.;
+
+      auto command = Accelerator::makeCommand(queue);
+      auto in_out_rhs_variable_na = Accelerator::viewInOut(command, rhs_variable_na);
+      auto in_node_coord = Accelerator::viewIn(command, node_coord);
+      FaceInfoListView faces_infos(mesh->faceFamily());
+      command << RUNCOMMAND_ENUMERATE(FaceLocalId, face_lid, group)
+      {
+        Real length = MeshOperation::computeLengthFace(face_lid, fn_cv, in_node_coord);
+        Real2 normal = MeshOperation::computeNormalFace(face_lid, fn_cv, in_node_coord, faces_infos);
+        for (NodeLocalId node_lid : fn_cv.nodes(face_lid)) {
+          if (nodes_infos.isOwn(node_lid)) {
+            Real rhs_value = (normal.x * value_x + normal.y * value_y) * length / 2.;
+            Accelerator::doAtomic<Accelerator::eAtomicOperation::Add>(in_out_rhs_variable_na[node_dof.dofId(node_lid, 0)], rhs_value);
+          }
+        }
+      };
+    }
+  }
+}
 
 } // namespace Arcane::FemUtils::Gpu::BoundaryConditions2D
 
