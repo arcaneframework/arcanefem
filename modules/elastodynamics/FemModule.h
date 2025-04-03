0ee978769cc925091cfc140dccae4d3efd396681
mohd-afeef-badri <afeef.badri@gmail.com>
2025-04-03 14:28:46 +0200

    Initilize BSR matrix

--- modules/elastodynamics/FemModule.h
+++ modules/elastodynamics/FemModule.h
@@ -31,6 +31,7 @@
 #include "FemUtils.h"
 #include "DoFLinearSystem.h"
 #include "FemDoFsOnNodes.h"
+#include "BSRFormat.h"
 #include "ArcaneFemFunctions.h"
 #include "ArcaneFemFunctionsGpu.h"
 
@@ -54,6 +55,7 @@ class FemModule
   explicit FemModule(const ModuleBuildInfo& mbi)
   : ArcaneFemObject(mbi)
   , m_dofs_on_nodes(mbi.subDomain()->traceMng())
+  , m_bsr_format(mbi.subDomain()->traceMng(), *(mbi.subDomain()->acceleratorMng()->defaultQueue()), m_dofs_on_nodes)
   {
     ICaseMng* cm = mbi.subDomain()->caseMng();
     cm->setTreatWarningAsError(true);
@@ -111,6 +113,7 @@ class FemModule
 
   DoFLinearSystem m_linear_system;
   FemDoFsOnNodes m_dofs_on_nodes;
+  BSRFormat m_bsr_format;
 
   // Struct to make sure we are using a CaseTable associated
   // to the right file
