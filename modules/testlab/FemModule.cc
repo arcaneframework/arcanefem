447eae06c15ab70a3101b868e78605572c9b63c2
mohd-afeef-badri <afeef.badri@gmail.com>
2025-11-10 10:57:01 +0100

    wrong timer

--- modules/testlab/FemModule.cc
+++ modules/testlab/FemModule.cc
@@ -2015,9 +2015,6 @@ _solve()
   elapsedTime = platform::getRealTime() - TimeStart;
   _printArcaneFemTime("[ArcaneFem-Timer] solving-linear-system-3", elapsedTime);
 
-  elapsedTime = platform::getRealTime() - TimeStart;
-  _printArcaneFemTime("[ArcaneFem-Timer] solving-linear-system2", elapsedTime);
-
   // Re-Apply boundary conditions because the solver has modified the value
   {
     TimeStart = platform::getRealTime();
