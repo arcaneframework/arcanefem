2044664cda2a37c9fa9e2e3534fedef2c5182a7b
jojoasticot <johan.emmanuelli2004@gmail.com>
2025-11-27 11:01:48 +0100

    fix[femutis]: fixed segfault on mpi

--- femutils/PETScDoFLinearSystem.cc
+++ femutils/PETScDoFLinearSystem.cc
@@ -128,7 +128,7 @@ class PETScDoFLinearSystemImpl
 
   void _computeMatrixNumeration(MPI_Comm mpi_comm);
   void _handleParameters(IParallelMng* pm);
-  NumArray<PetscInt, MDDim1> _CSRToCOO(Span<const int32_t> csr_rows, int32_t nb_value);
+  UniqueArray<PetscInt> _CSRToCOO(const Span<const int32_t> csr_rows, int32_t nb_value);
 };
 
 void PETScDoFLinearSystemImpl::_handleParameters(IParallelMng* pm)
@@ -239,9 +239,9 @@ _computeMatrixNumeration(MPI_Comm mpi_comm)
   m_rhs_work_values.resize(m_nb_own_row);
 }
 
-NumArray<PetscInt, MDDim1> PETScDoFLinearSystemImpl::_CSRToCOO(const Span<const int32_t> csr_rows, int32_t nb_value)
+UniqueArray<PetscInt> PETScDoFLinearSystemImpl::_CSRToCOO(const Span<const int32_t> csr_rows, int32_t nb_value)
 {
-  NumArray<PetscInt, MDDim1> coo_rows;
+  UniqueArray<PetscInt> coo_rows;
   coo_rows.resize(nb_value);
 
   for (int i = 0; i < csr_rows.size() - 1; i++) {
@@ -294,6 +294,7 @@ solve()
   if (true || is_parallel) // TODO sequential case will work with a sorted CSR implementation
   {
     DoFGroup all_dofs = dof_family->allItems();
+
     NumArray<PetscInt, MDDim1> indices{ all_dofs.size() };
     // info() << "nb total row " << m_nb_total_row;
 
@@ -302,10 +303,11 @@ solve()
       // info() << "local index: " << idof.index() << " global index: " << indices[idof.index()];
     }
 
-    PetscCallAbort(mpi_comm, ISLocalToGlobalMappingCreate(mpi_comm, 1, all_dofs.size(), indices._internalData(), PETSC_COPY_VALUES, &m_petsc_map));
+    PetscCallAbort(mpi_comm, ISLocalToGlobalMappingCreate(MPI_COMM_WORLD, 1, all_dofs.size(), indices._internalData(), PETSC_COPY_VALUES, &m_petsc_map));
 
     // info() << "Total " << m_nb_total_row << " local: " << m_nb_own_row;
     pm->barrier();
+
     PetscCallAbort(mpi_comm, MatCreate(mpi_comm, &m_petsc_matrix));
     PetscCallAbort(mpi_comm, MatSetSizes(m_petsc_matrix, local_rows, local_rows, global_rows, global_rows));
     PetscCallAbort(mpi_comm, MatSetFromOptions(m_petsc_matrix));
@@ -313,11 +315,12 @@ solve()
 
     // info() << "nb cols: " << csr_view.nbColumn() << ", nb rows: " << csr_view.nbRow() << ", nb vals: " << csr_view.nbValue();
 
-    NumArray<PetscInt, MDDim1> coo_rows = _CSRToCOO(csr_view.rows(), csr_view.nbValue());
-    NumArray<PetscInt, MDDim1> coo_cols;
+    UniqueArray<PetscInt> coo_rows = _CSRToCOO(csr_view.rows(), csr_view.nbValue());
+    UniqueArray<PetscInt> coo_cols;
+    // coo_cols.assign(csr_view.columns().begin(), csr_view.columns().end());
     coo_cols.copy(csr_view.columns()); // copy column array
 
-    PetscCallAbort(mpi_comm, MatSetPreallocationCOOLocal(m_petsc_matrix, csr_view.nbValue(), coo_rows._internalData(), coo_cols._internalData()));
+    PetscCallAbort(mpi_comm, MatSetPreallocationCOOLocal(m_petsc_matrix, csr_view.nbValue(), coo_rows.data(), coo_cols.data()));
     PetscCallAbort(mpi_comm, MatSetValuesCOO(m_petsc_matrix, csr_view.values().data(), INSERT_VALUES));
   }
   else {
@@ -351,7 +354,7 @@ solve()
 
   Real b2 = platform::getRealTime();
 
-  info() << "[PETSc-Timer] Time to create matrix = " << (b2 - c1);
+  info() << "[PETSc-Timer] Time to create matrix = " << (c1 - b2);
 
   VariableDoFReal& rhs_variable = this->rhsVariable();
   VariableDoFReal& dof_variable = this->solutionVariable();
