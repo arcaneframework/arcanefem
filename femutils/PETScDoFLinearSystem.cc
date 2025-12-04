6f78aef36d25cbad0953a7e07ddcc0d2c53fa3d1
mohd-afeef-badri <afeef.badri@gmail.com>
2025-12-04 22:09:04 +0100

    add correct communicator for parallel manager

--- femutils/PETScDoFLinearSystem.cc
+++ femutils/PETScDoFLinearSystem.cc
@@ -301,8 +301,7 @@ solve()
     }
 
     ISLocalToGlobalMapping m_petsc_map;
-    // TODO replace MPI_COMM_WORLD with
-    PetscCallAbort(mpi_comm, ISLocalToGlobalMappingCreate(MPI_COMM_WORLD, 1, all_dofs.size(), indices._internalData(), PETSC_COPY_VALUES, &m_petsc_map));
+    PetscCallAbort(mpi_comm, ISLocalToGlobalMappingCreate(mpi_comm, 1, all_dofs.size(), indices._internalData(), PETSC_COPY_VALUES, &m_petsc_map));
 
     // info() << "Total " << m_nb_total_row << " local: " << m_nb_own_row;
     pm->barrier();
