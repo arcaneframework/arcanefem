879a12a1381c1f245b56dfaad44835f146b77b33
jojoasticot <johan.emmanuelli2004@gmail.com>
2025-12-17 11:19:46 +0100

    fix(femutils): added bool options for constant sparsity and constant values

--- femutils/PETScDoFLinearSystem.cc
+++ femutils/PETScDoFLinearSystem.cc
@@ -65,9 +65,14 @@ class PETScDoFLinearSystemImpl
     IItemFamily* dof_family = dofFamily();
     IParallelMng* pm = dof_family->parallelMng();
     MPI_Comm mpi_comm = static_cast<MPI_Comm>(pm->communicator());
-    PetscCallAbort(mpi_comm, ISLocalToGlobalMappingDestroy(&m_petsc_map));
-    PetscCallAbort(mpi_comm, MatDestroy(&m_petsc_matrix));
-    PetscCallAbort(mpi_comm, KSPDestroy(&m_petsc_solver_context));
+
+    if (m_constant_matrix_sparsity)
+    {
+      PetscCallAbort(mpi_comm, ISLocalToGlobalMappingDestroy(&m_petsc_map));
+      PetscCallAbort(mpi_comm, MatDestroy(&m_petsc_matrix));
+      PetscCallAbort(mpi_comm, KSPDestroy(&m_petsc_solver_context));
+    }
+
     PetscFinalize();
   }
 
@@ -107,7 +112,10 @@ class PETScDoFLinearSystemImpl
   Vec m_petsc_rhs_vector;
   Mat m_petsc_matrix;
   ISLocalToGlobalMapping m_petsc_map;
+
   bool m_is_initialized = false;
+  bool m_constant_matrix_sparsity = true;
+  bool m_constant_matrix_values = true;
 
  private:
 
@@ -304,7 +312,6 @@ void PETScDoFLinearSystemImpl::_preallocateMatrix()
   coo_cols.copy(csr_view.columns()); // copy column array
 
   PetscCallAbort(mpi_comm, MatSetPreallocationCOOLocal(m_petsc_matrix, csr_view.nbValue(), coo_rows.data(), coo_cols.data()));
-
   PetscCallAbort(mpi_comm, MatAssemblyBegin(m_petsc_matrix, MAT_FINAL_ASSEMBLY));
   PetscCallAbort(mpi_comm, MatAssemblyEnd(m_petsc_matrix, MAT_FINAL_ASSEMBLY));
 
@@ -319,9 +326,21 @@ void PETScDoFLinearSystemImpl::_initSolve()
   IParallelMng* pm = dof_family->parallelMng();
   Runner runner = this->runner();
   MPI_Comm mpi_comm = static_cast<MPI_Comm>(pm->communicator());
+  CSRFormatView csr_view = this->getCSRValues();
+
   _handleParameters(pm);
   _computeMatrixNumeration(mpi_comm);
-  _preallocateMatrix();
+
+  if (m_constant_matrix_sparsity)
+    _preallocateMatrix();
+
+  if (m_constant_matrix_values)
+  {
+    PetscCallAbort(mpi_comm, MatSetValuesCOO(m_petsc_matrix, csr_view.values().data(), INSERT_VALUES));
+    PetscCallAbort(mpi_comm, MatAssemblyBegin(m_petsc_matrix, MAT_FINAL_ASSEMBLY));
+    PetscCallAbort(mpi_comm, MatAssemblyEnd(m_petsc_matrix, MAT_FINAL_ASSEMBLY));
+  }
+
   m_is_initialized = true;
 }
 
@@ -358,43 +377,17 @@ solve()
 
   // TODO: use COO with MatSetPreallocationCOO for better performance
   // TODO: see if we pass pointers to device memory directly when is_use_device==true
+  if (!m_constant_matrix_sparsity)
+    _preallocateMatrix();
+
+  if (!m_constant_matrix_values)
   {
-    // DoFGroup all_dofs = dof_family->allItems();
-
-    // NumArray<PetscInt, MDDim1> indices{ all_dofs.size() };
-    // // info() << "nb total row " << m_nb_total_row;
-
-    // ENUMERATE_DOF (idof, all_dofs) {
-    //   indices[idof.index()] = m_dof_matrix_numbering[idof];
-    //   // info() << "local index: " << idof.index() << " global index: " << indices[idof.index()];
-    // }
-
-    // PetscCallAbort(mpi_comm, ISLocalToGlobalMappingCreate(mpi_comm, 1, all_dofs.size(), indices._internalData(), PETSC_COPY_VALUES, &m_petsc_map));
-
-    // // info() << "Total " << m_nb_total_row << " local: " << m_nb_own_row;
-    // pm->barrier();
-
-    // PetscCallAbort(mpi_comm, MatCreate(mpi_comm, &m_petsc_matrix));
-    // PetscCallAbort(mpi_comm, MatSetSizes(m_petsc_matrix, local_rows, local_rows, global_rows, global_rows));
-    // PetscCallAbort(mpi_comm, MatSetFromOptions(m_petsc_matrix));
-    // PetscCallAbort(mpi_comm, MatSetLocalToGlobalMapping(m_petsc_matrix, m_petsc_map, m_petsc_map));
-
-    // // info() << "nb cols: " << csr_view.nbColumn() << ", nb rows: " << csr_view.nbRow() << ", nb vals: " << csr_view.nbValue();
-
-    // UniqueArray<PetscInt> coo_rows = _CSRToCOO(csr_view.rows(), csr_view.nbValue());
-    // UniqueArray<PetscInt> coo_cols;
-    // // coo_cols.assign(csr_view.columns().begin(), csr_view.columns().end());
-    // coo_cols.copy(csr_view.columns()); // copy column array
-
-    // PetscCallAbort(mpi_comm, MatSetPreallocationCOOLocal(m_petsc_matrix, csr_view.nbValue(), coo_rows.data(), coo_cols.data()));
-    // _preallocateMatrix();
     PetscCallAbort(mpi_comm, MatSetValuesCOO(m_petsc_matrix, csr_view.values().data(), INSERT_VALUES));
+    PetscCallAbort(mpi_comm, MatAssemblyBegin(m_petsc_matrix, MAT_FINAL_ASSEMBLY));
+    PetscCallAbort(mpi_comm, MatAssemblyEnd(m_petsc_matrix, MAT_FINAL_ASSEMBLY));
   }
 
   pm->barrier();
-
-  PetscCallAbort(mpi_comm, MatAssemblyBegin(m_petsc_matrix, MAT_FINAL_ASSEMBLY));
-  PetscCallAbort(mpi_comm, MatAssemblyEnd(m_petsc_matrix, MAT_FINAL_ASSEMBLY));
 
   Real b2 = platform::getRealTime();
 
@@ -488,6 +481,13 @@ solve()
 
   info() << "[PETSc-Info] Wrote solution in solution_variable";
   info() << "[PETSc-Info] Device memory allocation (Mo): " << (a.totalMemory() - a.freeMemory()) / 1e6;
+
+  if (!m_constant_matrix_sparsity)
+  {
+    PetscCallAbort(mpi_comm, ISLocalToGlobalMappingDestroy(&m_petsc_map));
+    PetscCallAbort(mpi_comm, MatDestroy(&m_petsc_matrix));
+    PetscCallAbort(mpi_comm, KSPDestroy(&m_petsc_solver_context));
+  }
 
   PetscCallAbort(mpi_comm, VecDestroy(&m_petsc_solution_vector));
   PetscCallAbort(mpi_comm, VecDestroy(&m_petsc_rhs_vector));
