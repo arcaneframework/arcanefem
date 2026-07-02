728c135bf872e7864671e0542c78cd7055e29e78
A-Tolan <andreea.tolan@ens-paris-saclay.fr>
2026-07-02 16:28:18 +0200

    Add quad 9 matrix and gradient jacobian calculations

--- modules/poisson/FemModule.h
+++ modules/poisson/FemModule.h
@@ -113,6 +113,7 @@ class FemModulePoisson
   RealMatrix<4, 4> _computeElementMatrixTetra4(Cell cell);
   RealMatrix<4, 4> _computeElementMatrixQuad4(Cell cell);
   RealMatrix<8, 8> _computeElementMatrixQuad8(Cell cell);
+  RealMatrix<9, 9> _computeElementMatrixQuad9(Cell cell);
   RealMatrix<8, 8> _computeElementMatrixHexa8(Cell cell);
   template <int N>
   void _assembleBilinear(const std::function<RealMatrix<N, N>(const Cell&)>& compute_element_matrix);
