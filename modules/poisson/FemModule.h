18129ea316706ab50d2951589ea1de9d2727f630
A-Tolan <andreea.tolan@ens-paris-saclay.fr>
2026-07-23 15:28:42 +0200

    add Hexa20 and Hexa27 matrix and gradient jacobian calculations, also corrected some comment bugs

--- modules/poisson/FemModule.h
+++ modules/poisson/FemModule.h
@@ -126,6 +126,8 @@ class FemModulePoisson
   RealMatrix<8, 8> _computeElementMatrixQuad8(Cell cell);
   RealMatrix<9, 9> _computeElementMatrixQuad9(Cell cell);
   RealMatrix<8, 8> _computeElementMatrixHexa8(Cell cell);
+  RealMatrix<20, 20> _computeElementMatrixHexa20(Cell cell);
+  RealMatrix<27, 27> _computeElementMatrixHexa27(Cell cell);
   template <int N>
   void _assembleBilinear(const std::function<RealMatrix<N, N>(const Cell&)>& compute_element_matrix);
 };
