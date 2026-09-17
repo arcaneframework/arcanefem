b2f3b40d020abecedc5f742520faa2615d3f4bdf
aliasad477 <syedaliasad2010@gmail.com>
2026-09-17 10:27:42 +0200

    fix(elastoplasticity): Base GPu function used for GPU assembly of matrix for Von Mises.

--- modules/elastoplasticity/VonMisesLaw.h
+++ modules/elastoplasticity/VonMisesLaw.h
@@ -268,70 +268,54 @@ inline void FemModuleElastoplasticity::_updateGlobalTangentMaterialTensorVonMise
   command << RUNCOMMAND_ENUMERATE(CellLocalId, cell_lid, mesh()->allCells())
   {
     for (Int8 iGP = 0; iGP < in_nGP; ++iGP ) {
-      // --- compute_trial_state ---- //
+      // read gp variables //
+      RealMatrix<3, 3> C_tang_gp;
+      C_tang_gp(0, 0) = in_out_C_tang_gp(cell_lid, iGP, 0, 0);
+      C_tang_gp(0, 1) = in_out_C_tang_gp(cell_lid, iGP, 0, 1);
+      C_tang_gp(0, 2) = in_out_C_tang_gp(cell_lid, iGP, 0, 2);
+      C_tang_gp(1, 0) = in_out_C_tang_gp(cell_lid, iGP, 1, 0);
+      C_tang_gp(1, 1) = in_out_C_tang_gp(cell_lid, iGP, 1, 1);
+      C_tang_gp(1, 2) = in_out_C_tang_gp(cell_lid, iGP, 1, 2);
+      C_tang_gp(2, 0) = in_out_C_tang_gp(cell_lid, iGP, 2, 0);
+      C_tang_gp(2, 1) = in_out_C_tang_gp(cell_lid, iGP, 2, 1);
+      C_tang_gp(2, 2) = in_out_C_tang_gp(cell_lid, iGP, 2, 2);
+      RealVector<3> sigma_gp;
+      sigma_gp(0) = in_out_sigma_gp(cell_lid, iGP, 0);
+      sigma_gp(1) = in_out_sigma_gp(cell_lid, iGP, 1);
+      sigma_gp(2) = in_out_sigma_gp(cell_lid, iGP, 2);
+      Real sigma_zz_gp = in_out_sigma_zz_gp(cell_lid, iGP);
+
+      RealVector<3> sigma_old_gp;
+      sigma_old_gp(0) = in_sigma_old_gp(cell_lid, iGP, 0);
+      sigma_old_gp(1) = in_sigma_old_gp(cell_lid, iGP, 1);
+      sigma_old_gp(2) = in_sigma_old_gp(cell_lid, iGP, 2);
+      Real sigma_zz_old_gp = in_sigma_zz_old_gp(cell_lid, iGP);
+
+      Real dp_gp = in_out_dp_gp(cell_lid, iGP);
+      Real p_old_gp = in_p_old_gp(cell_lid, iGP);
+
       // epsilon(DU) // NOTE: for nGP>1 it has to evaluated and interpolated at Gauss points
       Real3x3 grad_DU = Gpu::FeOperation2D::computeGradientTria3(cell_lid, cn_cv, in_node_coord, in_DUn);
-      Real eps_xx = grad_DU(0, 0);
-      Real eps_yy = grad_DU(1, 1);
-      Real eps_xy = 0.70710678118654752440 * (grad_DU(0, 1) + grad_DU(1, 0));
 
-      Real sigma_trial_xx = in_sigma_old_gp(cell_lid, iGP, 0) + in_C_elas_2d(0, 0) * eps_xx + in_C_elas_2d(0, 1) * eps_yy + in_C_elas_2d(0, 2) * eps_xy;
-      Real sigma_trial_yy = in_sigma_old_gp(cell_lid, iGP, 1) + in_C_elas_2d(1, 0) * eps_xx + in_C_elas_2d(1, 1) * eps_yy + in_C_elas_2d(1, 2) * eps_xy;
-      Real sigma_trial_xy = in_sigma_old_gp(cell_lid, iGP, 2) + in_C_elas_2d(2, 0) * eps_xx + in_C_elas_2d(2, 1) * eps_yy + in_C_elas_2d(2, 2) * eps_xy;
+      computeMaterialTensorVonMisesTria3Base(C_tang_gp, sigma_gp, sigma_zz_gp, dp_gp, grad_DU,
+                                              sigma_old_gp, sigma_zz_old_gp, p_old_gp,
+                                              in_C_elas_2d, in_sig0, in_H, in_mu);
 
-      Real sigma_trial_zz = in_sigma_zz_old_gp(cell_lid, iGP) + in_C_elas_2d(0, 1) * eps_yy + in_C_elas_2d(1, 0) * eps_xx;
-
-      // Plane strain retains sigma_zz in the three-dimensional deviator.
-      Real sigma_trial_mean = (sigma_trial_xx + sigma_trial_yy + sigma_trial_zz) / 3.0;
-
-      Real dev_xx = sigma_trial_xx - sigma_trial_mean;
-      Real dev_yy = sigma_trial_yy - sigma_trial_mean;
-      Real dev_xy = sigma_trial_xy;
-
-      Real dev_zz = sigma_trial_zz - sigma_trial_mean;
-
-      Real sigma_eq_trial = math::sqrt(1.5 * (dev_xx * dev_xx + dev_yy * dev_yy + dev_zz * dev_zz + dev_xy * dev_xy) );
-
-      // --- evaluate_yield_function ---- //
-      Real yield_function = sigma_eq_trial - in_sig0 - in_H * in_p_old_gp(cell_lid, iGP);
-      Real yield_positive = (yield_function + math::abs(yield_function)) / 2.;
-      in_out_dp_gp(cell_lid, iGP) = yield_positive/ (3. * in_mu + in_H);
-      Real plastic_switch = yield_positive / (math::abs(yield_function) + 1e-14 * in_sig0);
-
-      // --- radial_return_update ---- //
-      Real flowN_xx = plastic_switch * dev_xx / (sigma_eq_trial + 1e-14 * in_sig0);
-      Real flowN_yy = plastic_switch * dev_yy / (sigma_eq_trial + 1e-14 * in_sig0);
-      Real flowN_xy = plastic_switch * dev_xy / (sigma_eq_trial + 1e-14 * in_sig0);
-      // Real flowN_zz = plastic_switch * dev_zz / (sigma_eq_trial + 1e-14 * in_sig0);
-
-      Real beta = 3. * in_mu * in_out_dp_gp(cell_lid, iGP) / (sigma_eq_trial + 1e-14 * in_sig0);
-
-      // --- update_consistent_tangent ---- //
-      Real sigma_xx = sigma_trial_xx - dev_xx * beta;
-      Real sigma_yy = sigma_trial_yy - dev_yy * beta;
-      Real sigma_xy = sigma_trial_xy - dev_xy * beta;
-
-      Real sigma_zz = sigma_trial_zz - dev_zz * beta;
-
-      in_out_sigma_gp(cell_lid, iGP, 0) = sigma_xx;
-      in_out_sigma_gp(cell_lid, iGP, 1) = sigma_yy;
-      in_out_sigma_gp(cell_lid, iGP, 2) = sigma_xy;
-
-      in_out_sigma_zz_gp(cell_lid, iGP) = sigma_zz;
-
-      Real tangentA = 3.* in_mu * (3. * in_mu / (3. * in_mu + in_H) - beta);
-
-      in_out_C_tang_gp(cell_lid, iGP, 0, 0) = in_C_elas_2d(0, 0) - tangentA * flowN_xx * flowN_xx - 4. * in_mu * beta / 3.;
-      in_out_C_tang_gp(cell_lid, iGP, 0, 1) = in_C_elas_2d(0, 1) - tangentA * flowN_xx * flowN_yy + 2. * in_mu * beta / 3.;
-      in_out_C_tang_gp(cell_lid, iGP, 0, 2) = in_C_elas_2d(0, 2) - tangentA * flowN_xx * flowN_xy;
-
-      in_out_C_tang_gp(cell_lid, iGP, 1, 0) = in_out_C_tang_gp(cell_lid, iGP, 0, 1);
-      in_out_C_tang_gp(cell_lid, iGP, 1, 1) = in_C_elas_2d(1, 1) - tangentA * flowN_yy * flowN_yy - 4. * in_mu * beta / 3.;
-      in_out_C_tang_gp(cell_lid, iGP, 1, 2) = in_C_elas_2d(1, 2) - tangentA * flowN_yy * flowN_xy;
-
-      in_out_C_tang_gp(cell_lid, iGP, 2, 0) = in_out_C_tang_gp(cell_lid, iGP, 0, 2);
-      in_out_C_tang_gp(cell_lid, iGP, 2, 1) = in_out_C_tang_gp(cell_lid, iGP, 1, 2);
-      in_out_C_tang_gp(cell_lid, iGP, 2, 2) = in_C_elas_2d(2, 2) - tangentA * flowN_xy * flowN_xy - 2. * in_mu * beta;
+      // update gp variables //
+      in_out_C_tang_gp(cell_lid, iGP, 0, 0) = C_tang_gp(0, 0);
+      in_out_C_tang_gp(cell_lid, iGP, 0, 1) = C_tang_gp(0, 1);
+      in_out_C_tang_gp(cell_lid, iGP, 0, 2) = C_tang_gp(0, 2);
+      in_out_C_tang_gp(cell_lid, iGP, 1, 0) = C_tang_gp(1, 0);
+      in_out_C_tang_gp(cell_lid, iGP, 1, 1) = C_tang_gp(1, 1);
+      in_out_C_tang_gp(cell_lid, iGP, 1, 2) = C_tang_gp(1, 2);
+      in_out_C_tang_gp(cell_lid, iGP, 2, 0) = C_tang_gp(2, 0);
+      in_out_C_tang_gp(cell_lid, iGP, 2, 1) = C_tang_gp(2, 1);
+      in_out_C_tang_gp(cell_lid, iGP, 2, 2) = C_tang_gp(2, 2);
+      in_out_sigma_gp(cell_lid, iGP, 0) = sigma_gp(0);
+      in_out_sigma_gp(cell_lid, iGP, 1) = sigma_gp(1);
+      in_out_sigma_gp(cell_lid, iGP, 2) = sigma_gp(2);
+      in_out_sigma_zz_gp(cell_lid, iGP) = sigma_zz_gp;
+      in_out_dp_gp(cell_lid, iGP) = dp_gp;
     }
   };
 }
